deae7a71aea132854db3ae4d88156b650d1d9c2d
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2018-04-03 07:48:55 +0200

    swap out functions into utils.py

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 
 Naming a startup is [hard](https://mashable.com/2012/10/04/startup-naming/). 
 
-I thus wrote a Python script to randomly generate company name ideas.
+I therefore wrote a Python script to randomly generate company name ideas.
 
 It takes an arbitrary text as input, and then trains a recurrent neural network (RNN) on each its words, learning the structure of the text. The input text can be a simple word list (e.g. Greek or Gallic), or a chapter from a book, or just a random list of words (e.g. all Pokemon). The script then generates new random names that sound simliar to the provided list.
 
-I provided a few input texts in the `wordlists` directory - see the `README.md` there for descriptions.
+I provided a few input texts in the `wordlists` directory - see the `README.md` there for descriptions. I also added a Jupyter Notebook here that walks you through the inner workings, if you want to experiment yourself.
 
 You can (and maybe should) save a trained model in the `models` subdirectory, so that you don't have to train it every time you want to generate new names.
+
+### Detailed description and tutorial
+
+TODO blogpost
 
 ### Literature/References
 
@@ -18,12 +22,17 @@ You can (and maybe should) save a trained model in the `models` subdirectory, so
 
 ### Usage
 
-Show a brief help:
-    generate.py -h
+- Show a brief help:
 
+```python
+./generate.py -h
+```
 
-Sample usage, appending a custom, fixed suffix to each name:
-    ./generate.py -n 5 -t 0.7 -m models/behemoth_500epochs.h5 wordlists/behemoth.txt | sed -e 's/$/ Labs/g'
+- Sample usage, appending a custom, fixed suffix to each name:
+
+```python
+./generate.py -n 5 -t 0.7 -m models/behemoth_500epochs.h5 wordlists/behemoth.txt --suffix Labs
+```
 
 ```
 Artered Labs
--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from utils import *
 import argparse
 import os, sys
 import numpy as np
@@ -13,7 +14,9 @@ parser = argparse.ArgumentParser(description = "A Python script that generates s
                                  "the structure of the words, and finally outputs a list of suggestions with "
                                  "a similar structure as the words in the provided wordlist.")
 parser.add_argument("wordlist",
-                    help="Path to the word list, a not necessarily well-formatted .txt file")
+                    help="Path to the word list, a not necessarily well-formatted .txt file. "
+                    "You have to provide it even if you load a pre-trained model, because "
+                    "you currently need it to compute the list of possible characters to sample from.")
 parser.add_argument("-s", "--savepath",
                     help="Path to save the computed model")
 parser.add_argument("-m", "--modelpath",
@@ -28,6 +31,8 @@ parser.add_argument("-e", "--epochs", type = int, default = 100,
                     help="Number of epochs to train the model. Default 100, but try up to 500")
 parser.add_argument("-v", "--verbose",
                     help="Report more details", action = "store_true")
+parser.add_argument("--suffix",
+                    help="Suffix to put after the generated company name")
 
 args = parser.parse_args()
 
@@ -51,9 +56,6 @@ from keras.layers import Dense, Activation
 from keras.layers import LSTM, SimpleRNN, GRU, TimeDistributed
 from keras.callbacks import LambdaCallback
 
-from preprocess import text_to_words
-
-   
 # Generate a list of words (including newline)
 words = text_to_words(args.wordlist)
 
@@ -63,6 +65,7 @@ chars = sorted(list(set([char for word in words for char in word])))
 
 VOCAB_SIZE = len(chars)
 N_WORDS = len(words)  # #words in corpus - not #words to generate
+MIN_WORD_LEN = 4   # minimum company name length
 MAX_WORD_LEN = 12  # maximum company name length
 
 if args.verbose:
@@ -75,100 +78,67 @@ if args.verbose:
 ix_to_char = {ix:char for ix, char in enumerate(chars)}
 char_to_ix = {char:ix for ix, char in enumerate(chars)}
 
-X = np.zeros((N_WORDS, MAX_WORD_LEN, VOCAB_SIZE))
-Y = np.zeros((N_WORDS, MAX_WORD_LEN, VOCAB_SIZE))
-
-for word_i in range(N_WORDS):
-    word = words[word_i]
-    chars = list(word)
-    word_len = len(word)
-    
-    for char_j in range(min(len(word), MAX_WORD_LEN)):
-        char = chars[char_j]
-        char_ix = char_to_ix[char]
-        X[word_i, char_j, char_ix] = 1
-        if char_j > 0:
-            Y[word_i, char_j - 1, char_ix] = 1  # the 'next char' at time point char_j
-
-LAYER_NUM = 2
-HIDDEN_DIM = 50
-
-model = Sequential()
-model.add(LSTM(HIDDEN_DIM, input_shape=(None, VOCAB_SIZE), return_sequences=True))
-for i in range(LAYER_NUM - 1):
-    model.add(LSTM(HIDDEN_DIM, return_sequences=True))
-model.add(TimeDistributed(Dense(VOCAB_SIZE)))
-model.add(Activation('softmax'))
-model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
-
-def temp_scale(probs, temperature = 1.0):
-    # a low temperature (< 1 and approaching 0) results in the char sampling approaching the argmax.
-    # a high temperature (> 1, approaching infinity) results in sampling from a uniform distribution)
-    probs = np.exp(np.log(probs) / temperature)
-    probs = probs / np.sum(probs)
-    return probs
-    
-    
-def generate_word(model, temperature = 1.0, min_word_length = 4):
-    X = np.zeros((1, MAX_WORD_LEN, VOCAB_SIZE))
-    
-    # sample the first character
-    initial_char_distribution = temp_scale(model.predict(X[:, 0:1, :]).flatten(), temperature)
-    
-    ix = 0
-    while ix == 0:  # make sure the initial character is not a newline (i.e. index 0)
-        ix = int(np.random.choice(VOCAB_SIZE, size = 1, p = initial_char_distribution))
-    
-    X[0, 0, ix] = 1
-    generated_word = [ix_to_char[ix].upper()]  # start with first character, then later successively append chars
-    
-    # sample all remaining characters
-    for i in range(1, MAX_WORD_LEN):
-        next_char_distribution = temp_scale(model.predict(X[:, 0:i, :])[:, i-1, :].flatten(), temperature)
-
-        
-        ix_choice = np.random.choice(VOCAB_SIZE, size = 1, p = next_char_distribution)
-        # ix_choice = np.argmax(next_char_distribution)  # <- corresponds to sampling with a very low temperature
-        ctr = 0
-        while ix_choice == 0 and i < min_word_length:
-            ctr += 1
-            # sample again if you picked the end-of-word token too early
-            ix_choice = np.random.choice(VOCAB_SIZE, size = 1, p = next_char_distribution)
-            if ctr > 1000:
-                print("caught in a near-infinite loop. You might have picked too low a temperature "
-                      "and the sampler just keeps sampling \\n's")
-                break
-            
-        
-        next_ix = int(ix_choice)
-        X[0, i, next_ix] = 1
-        if next_ix == 0:
-            break
-        generated_word.append(ix_to_char[next_ix])
-    
-    return ('').join(generated_word)
-
-
+# TODO how to move this function into utils.py? If I do, execution fails because of "args.verbose not found"    
 def on_epoch_end(epoch, logs):
-    if epoch % 50 == 0 and args.verbose:
-        print("epoch " + str(epoch) + ": " + generate_word(model, temperature = 1.0, min_word_length = 4))
+    if epoch % 10 == 0 and args.verbose:
+        print("epoch " + str(epoch) + ": ", end="")
+        for _ in range(4):
+            word = generate_word(model,
+                                 ix_to_char = ix_to_char,
+                                 temperature = 1.0,
+                                 min_word_length = MIN_WORD_LEN,
+                                 max_word_length = MAX_WORD_LEN)
+            print(word + ", ", end="")
 
+        print("loss: " + str(np.round(logs['loss'], 4)))
 
-print_callback = LambdaCallback(on_epoch_end = on_epoch_end)
-
-NUM_EPOCHS = 500
-BATCH_SIZE = 64  # or: N_WORDS
 
 if args.modelpath != None:
-    ## Load one of these models if you have trained them before and want to skip re-training
+    # Load one of these models if you have trained them before
+    #  and want to skip re-training
     model = keras.models.load_model(args.modelpath)
 else:
-    model.fit(X, Y, batch_size = BATCH_SIZE, verbose = 0, epochs = NUM_EPOCHS, callbacks = [print_callback])
+    X = np.zeros((N_WORDS, MAX_WORD_LEN, VOCAB_SIZE))
+    Y = np.zeros((N_WORDS, MAX_WORD_LEN, VOCAB_SIZE))
+    for word_i in range(N_WORDS):
+        word = words[word_i]
+        chars = list(word)
+        word_len = len(word)
+    
+        for char_j in range(min(len(word), MAX_WORD_LEN)):
+            char = chars[char_j]
+            char_ix = char_to_ix[char]
+            X[word_i, char_j, char_ix] = 1
+            if char_j > 0:
+                Y[word_i, char_j - 1, char_ix] = 1  # the 'next char' at time point char_j
+
+    LAYER_NUM = 2
+    HIDDEN_DIM = 50
+
+    model = Sequential()
+    model.add(LSTM(HIDDEN_DIM, input_shape=(None, VOCAB_SIZE), return_sequences=True))
+    for i in range(LAYER_NUM - 1):
+        model.add(LSTM(HIDDEN_DIM, return_sequences=True))
+    model.add(TimeDistributed(Dense(VOCAB_SIZE)))
+    model.add(Activation('softmax'))
+    model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
+
+    print_callback = LambdaCallback(on_epoch_end = on_epoch_end)
+    BATCH_SIZE = 64
+    model.fit(X, Y, batch_size = BATCH_SIZE, verbose = 0,
+              epochs = args.epochs, callbacks = [print_callback])
 
     if args.savepath != None:
         model.save(args.savepath)
 
 # Print a few words with the final model:
-
+print("\n\nOutput:\n=======\n")
 for _ in range(args.nwords):
-    print(generate_word(model, temperature = args.temperature, min_word_length = 4))
+    name = generate_word(model,
+                         ix_to_char = ix_to_char,
+                         temperature = args.temperature,
+                         min_word_length = MIN_WORD_LEN,
+                         max_word_length = MAX_WORD_LEN)
+    if args.suffix != None:
+        name = name + " " + args.suffix
+    print(name)
