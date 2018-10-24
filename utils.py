9097d86c14ad4d9dd354fd9ad6adc259d021765f
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2018-10-24 17:19:17 +0200

    add prepared notebooks

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def text_to_words(textfile):
              for x in open(textfile)]
 
     all_words = [word for line in words for word in line]
-    
+
     unique_words = list(set(all_words))
 
     # finally, append a newline as end-of-word token to each word:
@@ -28,24 +28,24 @@ def temp_scale(probs, temperature = 1.0):
 def generate_word(model, ix_to_char, temperature = 1.0, min_word_length = 4, max_word_length = 12):
 
     VOCAB_SIZE = model.get_config()[0]['config']['batch_input_shape'][2]
-    
+
     X = np.zeros((1, max_word_length, VOCAB_SIZE))
-    
+
     # sample the first character
     initial_char_distribution = temp_scale(model.predict(X[:, 0:1, :]).flatten(), temperature)
-    
+
     ix = 0
     while ix == 0:  # make sure the initial character is not a newline (i.e. index 0)
         ix = int(np.random.choice(VOCAB_SIZE, size = 1, p = initial_char_distribution))
-    
+
     X[0, 0, ix] = 1
     generated_word = [ix_to_char[ix].upper()]  # start with first character, then later successively append chars
-    
+
     # sample all remaining characters
     for i in range(1, max_word_length):
         next_char_distribution = temp_scale(model.predict(X[:, 0:i, :])[:, i-1, :].flatten(), temperature)
 
-        
+
         ix_choice = np.random.choice(VOCAB_SIZE, size = 1, p = next_char_distribution)
         # ix_choice = np.argmax(next_char_distribution)  # <- corresponds to sampling with a very low temperature
         ctr = 0
@@ -57,14 +57,12 @@ def generate_word(model, ix_to_char, temperature = 1.0, min_word_length = 4, max
                 print("caught in a near-infinite loop. You might have picked too low a temperature "
                       "and the sampler just keeps sampling \\n's")
                 break
-            
-        
+
+
         next_ix = int(ix_choice)
         X[0, i, next_ix] = 1
         if next_ix == 0:
             break
         generated_word.append(ix_to_char[next_ix])
-    
+
     return ('').join(generated_word)
-
-
