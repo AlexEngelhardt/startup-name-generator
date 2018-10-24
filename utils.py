4fd53e1b9f9a14324ec0e1f75f8b1393c5b2cea3
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2018-10-24 19:49:48 +0200

    more prepared notebooks

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def text_to_words(textfile):
     from keras.preprocessing.text import text_to_word_sequence
 
@@ -17,47 +18,71 @@ def text_to_words(textfile):
     return unique_words
 
 
-def temp_scale(probs, temperature = 1.0):
-    # a low temperature (< 1 and approaching 0) results in the char sampling approaching the argmax.
-    # a high temperature (> 1, approaching infinity) results in sampling from a uniform distribution)
+def temp_scale(probs, temperature=1.0):
+    # a low temperature (< 1 and approaching 0) results in the char sampling
+    # approaching the argmax.  a high temperature (> 1, approaching infinity)
+    # results in sampling from a uniform distribution)
     probs = np.exp(np.log(probs) / temperature)
     probs = probs / np.sum(probs)
     return probs
 
 
-def generate_word(model, ix_to_char, temperature = 1.0, min_word_length = 4, max_word_length = 12):
+def generate_word(model, ix_to_char, temperature=1.0, min_word_length=4,
+                  max_word_length=12):
 
     VOCAB_SIZE = model.get_config()[0]['config']['batch_input_shape'][2]
 
     X = np.zeros((1, max_word_length, VOCAB_SIZE))
 
     # sample the first character
-    initial_char_distribution = temp_scale(model.predict(X[:, 0:1, :]).flatten(), temperature)
+    initial_char_distribution = temp_scale(
+        model.predict(X[:, 0:1, :]).flatten(),
+        temperature
+    )
 
     ix = 0
-    while ix == 0:  # make sure the initial character is not a newline (i.e. index 0)
-        ix = int(np.random.choice(VOCAB_SIZE, size = 1, p = initial_char_distribution))
+
+    # make sure the initial character is not a newline (i.e. index 0):
+    while ix == 0:
+        ix = int(
+            np.random.choice(VOCAB_SIZE, size=1, p=initial_char_distribution)
+        )
 
     X[0, 0, ix] = 1
-    generated_word = [ix_to_char[ix].upper()]  # start with first character, then later successively append chars
+
+    # start with first character, then later successively append chars:
+    generated_word = [ix_to_char[ix].upper()]
 
     # sample all remaining characters
     for i in range(1, max_word_length):
-        next_char_distribution = temp_scale(model.predict(X[:, 0:i, :])[:, i-1, :].flatten(), temperature)
+        next_char_distribution = temp_scale(
+            model.predict(X[:, 0:i, :])[:, i-1, :].flatten(),
+            temperature
+        )
 
+        ix_choice = np.random.choice(
+            VOCAB_SIZE,
+            size=1,
+            p=next_char_distribution
+        )
 
-        ix_choice = np.random.choice(VOCAB_SIZE, size = 1, p = next_char_distribution)
-        # ix_choice = np.argmax(next_char_distribution)  # <- corresponds to sampling with a very low temperature
+        # corresponds to sampling with a very low temperature:
+        # ix_choice = np.argmax(next_char_distribution)
+
         ctr = 0
         while ix_choice == 0 and i < min_word_length:
             ctr += 1
             # sample again if you picked the end-of-word token too early
-            ix_choice = np.random.choice(VOCAB_SIZE, size = 1, p = next_char_distribution)
+            ix_choice = np.random.choice(
+                VOCAB_SIZE,
+                size=1,
+                p=next_char_distribution
+            )
             if ctr > 1000:
-                print("caught in a near-infinite loop. You might have picked too low a temperature "
+                print("caught in a near-infinite loop. You might have "
+                      "picked too low a temperature "
                       "and the sampler just keeps sampling \\n's")
                 break
-
 
         next_ix = int(ix_choice)
         X[0, i, next_ix] = 1
